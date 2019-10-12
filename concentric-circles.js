f27b993286b67925293f7616f42e06d53021698a
JooPJoo <38052366+JooPJoo@users.noreply.github.com>
2019-10-12 00:52:52 -0700

    Create concentric-circles.js

---
+++ concentric-circles.js
@@ -0,0 +1,17 @@
+function setup() 
+  {
+    createCanvas(400, 400);
+    noFill();
+  }
+
+function draw() 
+  {
+    background(220);
+    ellipseMode(CENTER);
+    
+    for (var i = 10; i <= 390; i = i + 10) 
+      {
+        
+        ellipse(width/2, height/2, i);
+      }
+  }
